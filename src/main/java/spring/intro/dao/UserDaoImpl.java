44ca6967ce34175a3b8ed41b4b5c2ded85c33482
antoniukivan <ivan.antonuk1992@gmail.com>
2021-02-08 17:35:48 +0200

    add UserDao interface to the dao package

---
+++ src/main/java/spring/intro/dao/UserDaoImpl.java
@@ -0,0 +1,16 @@
+package spring.intro.dao;
+
+import java.util.List;
+import spring.intro.model.User;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public void add(User user) {
+
+    }
+
+    @Override
+    public List<User> listUsers() {
+        return null;
+    }
+}
