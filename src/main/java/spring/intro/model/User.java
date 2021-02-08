783c0ce1b8a759d78d5071a13864f05b5f995fae
antoniukivan <ivan.antonuk1992@gmail.com>
2021-02-08 17:35:04 +0200

    add User class to the model package

---
+++ src/main/java/spring/intro/model/User.java
@@ -0,0 +1,38 @@
+package spring.intro.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "id=" + id
+                + ", login='" + login + '\''
+                + '}';
+    }
+}
